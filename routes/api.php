c977e9de91860608a6523836cabf01547145d4f5
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-07-30 11:58:17 -0300

    feat(middleware): token validation by header

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ExamController;
 use App\Http\Controllers\Api\V1\PackageController;
+use App\Http\Middleware\EnsureTokenIsValid;
 
-Route::prefix('v1')->group(function () {
+Route::middleware([EnsureTokenIsValid::class])->prefix('v1')->group(function () {
     Route::get('exams', [ExamController::class, 'index']);
     Route::post('exams', [ExamController::class, 'store']);
 
