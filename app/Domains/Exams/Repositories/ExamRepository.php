ed03d30193c3936df77342b6a48db86862e379a2
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-07-30 11:23:14 -0300

    feat(repositories): ExamRepository e PackageRepository

--- app/Domains/Exams/Repositories/ExamRepository.php
+++ app/Domains/Exams/Repositories/ExamRepository.php
@@ -2,7 +2,22 @@
 
 namespace Domains\Exams\Repositories;
 
+use Domains\Exams\Models\Exam;
+
 class ExamRepository
 {
-    //
+    public function store(array $data): Exam
+    {
+        return Exam::create($data);
+    }
+
+    public function find(string $id): ?Exam
+    {
+        return Exam::find($id);
+    }
+
+    public function all()
+    {
+        return Exam::all();
+    }
 }
--- app/Domains/Packages/Repositories/PackageRepository.php
+++ app/Domains/Packages/Repositories/PackageRepository.php
@@ -1,8 +1,23 @@
 <?php
 
-namespace Domains\Package\Repositories;
+namespace Domains\Exams\Repositories;
+
+use Domains\Exams\Models\Package;
 
 class PackageRepository
 {
-    //
+    public function store(array $data): Package
+    {
+        return Package::create($data);
+    }
+
+    public function find(string $id): ?Package
+    {
+        return Package::find($id);
+    }
+
+    public function all()
+    {
+        return Package::all();
+    }
 }
