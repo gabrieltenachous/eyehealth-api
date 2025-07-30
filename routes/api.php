1873effcfd8e3d6ad4ab5f3f4135e34a81034706
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-07-30 11:54:49 -0300

    feat(routes): Versioned API with endpoints for exams and packages

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ExamController;
+use App\Http\Controllers\Api\V1\PackageController;
+
+Route::prefix('v1')->group(function () {
+    Route::get('exams', [ExamController::class, 'index']);
+    Route::post('exams', [ExamController::class, 'store']);
+
+    Route::get('packages', [PackageController::class, 'index']);
+    Route::post('packages', [PackageController::class, 'store']);
+});
