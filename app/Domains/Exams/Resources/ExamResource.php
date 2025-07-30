9a8d5b6838c7fa0c0cc99a883bdc73913010b415
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2025-07-30 11:38:25 -0300

    feat(resources): ExamResource e PackageResource

--- app/Domains/Exams/Resources/ExamResource.php
+++ app/Domains/Exams/Resources/ExamResource.php
@@ -2,7 +2,18 @@
 
 namespace Domains\Exams\Resources;
 
-class ExamResource
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExamResource extends JsonResource
 {
-    //
+    public function toArray($request): array
+    {
+        return [
+            'id'         => $this->id,
+            'name'       => $this->name,
+            'comment'    => $this->comment,
+            'laterality' => $this->laterality,
+            'group'      => $this->group,
+        ];
+    }
 }
--- app/Domains/Packages/Resources/PackageResource.php
+++ app/Domains/Packages/Resources/PackageResource.php
@@ -2,7 +2,17 @@
 
 namespace Domains\Packages\Resources;
 
-class PackageResource
+use Illuminate\Http\Resources\Json\JsonResource;
+use Domains\Exams\Resources\ExamResource;
+
+class PackageResource extends JsonResource
 {
-    //
+    public function toArray($request): array
+    {
+        return [
+            'id'    => $this->id,
+            'name'  => $this->name,
+            'exams' => ExamResource::collection($this->whenLoaded('exams')),
+        ];
+    }
 }
